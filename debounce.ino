a3f9c682a9e98ac8dc3ea5ccf7d791b5f21c3dc4
emodz <em3149@nyu.edu>
2018-03-08 08:55:06 -0500

    Update debounce.ino

--- debounce.ino
+++ debounce.ino
@@ -1,8 +1,8 @@
 
 const int ledPin = 13; 
 const int buttonPin = 2; 
-int buttonState = LOW; //if button is not pressed state is low
-int ledState = -1; //if negative led off
+bool buttonState = LOW; //if button is not pressed state is low
+bool ledState = -1; //if negative led off
 long lastDebounce = 0;  // the last time led was on
 long debounceDelay = 50;  //if the led flickers debounce time increases by 50 
  
@@ -21,12 +21,12 @@ void loop() {
     //press button
     if ( (buttonState == HIGH) && (ledState < 0) ) {
       digitalWrite(ledPin, HIGH); //turn LED on
-      ledState = -ledState; //state on
+      ledState = HIGH; //state on
       lastDebounce = millis(); //set time
     }
     else if ( (buttonState == HIGH) && (ledState > 0) ) {
       digitalWrite(ledPin, LOW); //turn LED off
-      ledState = -ledState; //state off
+      ledState = LOW; //state off
       lastDebounce = millis(); //set time
     }
   }
